807c88110689dcf1486635e96538221300019087
Francesco Santoro <francescosantoro@MacBook-Pro-di-Francesco.local>
2022-07-03 18:15:02 +0200

    set fade loader

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
 const loader = document.querySelector('.loader');
+const main = document.querySelector('.main');
+
+
 
 function init() {
     setTimeout(() => {
-      loader.style.opacity = 0;
+      loader.style.opacity = 100;
       loader.style.display = 'none';
+
+      main.style.display = 'flex';
+      setTimeout (() => (main.style.opacity = 1), 50);
     }, 1300);
   }
 init();
