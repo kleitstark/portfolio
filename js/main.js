83673af051b950d7efe4efbececeff015566db9d
kleitstark <99267787+kleitstark@users.noreply.github.com>
2024-02-26 18:21:39 +0100

    add finally parallax effect!!

--- js/main.js
+++ js/main.js
@@ -32,7 +32,7 @@ const myText = new AutoTyping({
   id: 'typed',
   typeText: ["Francesco Santoro  ","a Software Developer  ", "a Musician  ", "a Drummer  "],
   textColor: '#fdd228',
-  cursor: '.',
+  cursor: '_',
   cursorColor:'#fff',
   deleteSpeed: 50,
   }).init();
