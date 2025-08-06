19acf617d1acce0a6aa2ba9b569a843f854c5f44
kleitstark <99267787+kleitstark@users.noreply.github.com>
2025-08-07 00:19:43 +0200

    new img & prettier

--- js/main.js
+++ js/main.js
@@ -8,15 +8,13 @@ function closeLoader() {
   setTimeout(() => {
     const loader = document.querySelector('.loader');
     loader.style.display = 'none';
-  },1200);
+  }, 1200);
 }
-closeLoader()
+closeLoader();
 
-particlesJS.load('particles-js', 'assets/particles.json', function() {
+particlesJS.load('particles-js', 'assets/particles.json', function () {
   console.log('callback - particles.js config loaded');
-});    
-
-
+});
 
 /*Typed String
 var typed = new Typed(".auto-type", {
@@ -30,51 +28,47 @@ var typed = new Typed(".auto-type", {
 
 const myText = new AutoTyping({
   id: 'typed',
-  typeText: ["Francesco Santoro  ","a Software Developer  ", "a Musician  ", "a Drummer  "],
+  typeText: ['Francesco Santoro  ', 'a Software Developer  ', 'a Musician  ', 'a Drummer  '],
   textColor: '#fdd228',
   cursor: '_',
-  cursorColor:'#fff',
+  cursorColor: '#fff',
   deleteSpeed: 50,
-  }).init();
-
+}).init();
 
 //active nav
 
 $(document).on('click', 'ul li a', () => {
-  $(this).addClass('active').siblings().removeClass('active')
+  $(this).addClass('active').siblings().removeClass('active');
 });
-
-
 
 //sticky nav on scroll
 
-$(window).scroll(function() {    
+$(window).scroll(function () {
   var scroll = $(window).scrollTop();
-  var x = $( "#click" );
-  if ($(x).prop("checked") == false && scroll < 30) {
-      $("#header").removeClass("sticky");
-  } else if ($(x).prop("checked") == false && scroll > 30) {
-    $("#header").addClass("sticky");
-  } 
-}); 
+  var x = $('#click');
+  if ($(x).prop('checked') == false && scroll < 30) {
+    $('#header').removeClass('sticky');
+  } else if ($(x).prop('checked') == false && scroll > 30) {
+    $('#header').addClass('sticky');
+  }
+});
 
 //!!!!!!!!!!!
 
-$("#click").change( function() {
+$('#click').change(function () {
   var scroll = $(window).scrollTop();
-  var y = $( "#click" );
-  if ($(y).is(':checked')){
-    $("#header").removeClass("sticky");
-    $(".fas").css("color", "#fede28");
+  var y = $('#click');
+  if ($(y).is(':checked')) {
+    $('#header').removeClass('sticky');
+    $('.fas').css('color', '#fede28');
   } else if ($(y).not(':checked') && scroll < 30) {
-    $("#header").removeClass("sticky");
-    $(".fas").css("color", "white");
+    $('#header').removeClass('sticky');
+    $('.fas').css('color', 'white');
   } else if ($(y).not(':checked') && scroll > 30) {
-    $("#header").addClass("sticky");
-    $(".fas").css("color", "white");
+    $('#header').addClass('sticky');
+    $('.fas').css('color', 'white');
   }
-})
-
+});
 
 /*hide show social icon 
 $("#click").change( function() {
@@ -87,32 +81,31 @@ $("#click").change( function() {
 })
 */
 
-
 //active li on scroll
 
-$(document).ready(function() {
+$(document).ready(function () {
   // Funzione per aggiungere la classe active agli elementi della navbar
   function setActiveNav() {
-      var scrollPosition = $(document).scrollTop();
-      console.log(scrollPosition)
+    var scrollPosition = $(document).scrollTop();
+    console.log(scrollPosition);
 
-      // Verifica la posizione dello scroll e aggiungi la classe active di conseguenza
-      if (scrollPosition < 807) {
-          $('#menu li').removeClass('active'); // Rimuovi la classe active da tutti gli elementi della navbar
-          $('#menu li a[href="#home"]').parent().addClass('active'); // Aggiungi la classe active all'elemento della navbar corrispondente
-      } else if (scrollPosition >= 807 && scrollPosition < 1915) {
-          $('#menu li').removeClass('active');
-          $('#menu li a[href="#about"]').parent().addClass('active');
-      } else if (scrollPosition >= 1915 && scrollPosition < 3640) {
-        $('#menu li').removeClass('active');
-        $('#menu li a[href="#resumes"]').parent().addClass('active');
-      } else if (scrollPosition >= 3640 && scrollPosition < 3654) {
-        $('#menu li').removeClass('active');
-        $('#menu li a[href="#content"]').parent().addClass('active');
-      } else if (scrollPosition >= 3654) {
-        $('#menu li').removeClass('active');
-        $('#menu li a[href="#contact"]').parent().addClass('active');
-      } 
+    // Verifica la posizione dello scroll e aggiungi la classe active di conseguenza
+    if (scrollPosition < 807) {
+      $('#menu li').removeClass('active'); // Rimuovi la classe active da tutti gli elementi della navbar
+      $('#menu li a[href="#home"]').parent().addClass('active'); // Aggiungi la classe active all'elemento della navbar corrispondente
+    } else if (scrollPosition >= 807 && scrollPosition < 1915) {
+      $('#menu li').removeClass('active');
+      $('#menu li a[href="#about"]').parent().addClass('active');
+    } else if (scrollPosition >= 1915 && scrollPosition < 3640) {
+      $('#menu li').removeClass('active');
+      $('#menu li a[href="#resumes"]').parent().addClass('active');
+    } else if (scrollPosition >= 3640 && scrollPosition < 3654) {
+      $('#menu li').removeClass('active');
+      $('#menu li a[href="#content"]').parent().addClass('active');
+    } else if (scrollPosition >= 3654) {
+      $('#menu li').removeClass('active');
+      $('#menu li a[href="#contact"]').parent().addClass('active');
+    }
   }
 
   // Attiva la funzione quando si scorre la pagina
@@ -126,60 +119,53 @@ window.addEventListener('scroll', function() {
 });
 */
 
-
-
 //collapse navbar  NICE JQUERY :)
 
-$(".link-page").on('click', function() {
-  $("#click").prop("checked", false);
-  $(".fas").css("color", "white");
-})
-
-
+$('.link-page').on('click', function () {
+  $('#click').prop('checked', false);
+  $('.fas').css('color', 'white');
+});
 
 //Nasconde hero per mostrare solo particles nella sezione carousel
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
   var scrollPosition = window.scrollY;
-  var heroElement = document.getElementById("hero"); 
+  var heroElement = document.getElementById('hero');
 
   if (scrollPosition > 700) {
-      if (heroElement) {
-          heroElement.style.opacity = 0;
-          heroElement.style.transition = 'opacity 0.3s ease'
-      }
+    if (heroElement) {
+      heroElement.style.opacity = 0;
+      heroElement.style.transition = 'opacity 0.3s ease';
+    }
   } else {
-      if (heroElement) {
-          heroElement.style.display = 'flex';
-          heroElement.style.opacity = 100;
-          heroElement.style.transition = 'opacity 0.5s ease'
-      }
+    if (heroElement) {
+      heroElement.style.display = 'flex';
+      heroElement.style.opacity = 100;
+      heroElement.style.transition = 'opacity 0.5s ease';
+    }
   }
 });
 
-
 /* To Top Button*/
-let backToTopButton = document.querySelector('.js-back-to-top-button')
+let backToTopButton = document.querySelector('.js-back-to-top-button');
 
 const onscroll = () => {
   if (window.scrollY > 1000) {
-    backToTopButton.classList.add('is-active')
+    backToTopButton.classList.add('is-active');
   } else {
-    backToTopButton.classList.remove('is-active')
+    backToTopButton.classList.remove('is-active');
   }
-}
+};
 
-window.addEventListener('scroll', onscroll)
+window.addEventListener('scroll', onscroll);
 
-scrollToTop =  () => {
+scrollToTop = () => {
   window.scrollTo({
     top: 0,
-    behavior: 'smooth'
-  })
-}
+    behavior: 'smooth',
+  });
+};
 
-backToTopButton.addEventListener('click', scrollToTop)
-
-
+backToTopButton.addEventListener('click', scrollToTop);
 
 /*
 $(window).scroll(function() {    
@@ -193,4 +179,4 @@ $(window).scroll(function() {
   } 
 });  
 
-*/ 
+*/
